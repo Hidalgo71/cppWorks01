3d3c2789f7874eb0cfdd1af42be8fba0ef7e4baf
Yekta Özdemir <lakers_yekta_ceza@hotmail.com>
2019-01-18 01:30:40 +0300

    new

--- cppWorks01/Source.cpp
+++ cppWorks01/Source.cpp
@@ -2,58 +2,16 @@
 
 using namespace std;
 
-class Base
+class Animal 
 {
-private:
-	int xValue = 11;
-	int yValue = 21;
-public:
-	Base()
-	{
-		cout << "Base Class Constructor." << endl;
-		cout << "X Value in Scope: " << xValue << endl;
-		cout << "Y Value in Scope: " << yValue << endl;
-	}
-	void setXvalue(int sXvalue)
-	{
-		xValue = sXvalue;
-	}
-	void setYvalue(int sYvalue)
-	{
-		yValue = sYvalue;
-	}
-	int getXvalue()
-	{
-		return xValue;
-	}
-	int getYvalue()
-	{
-		return yValue;
-	}
-};
-class Derived :public Base
-{
-public:
-	int newXvalue, newYvalue;
-	Derived()
-	{
-		cout << "Derived Class Constructor." << endl;		
-	}	
+
 };
 
-class NewClass
-{
-public:
-	NewClass()
-	{	
-		cout << "New Class Constructor" << endl;
-	}
-};
+
 
 int main()
 {
-	Base();
-	Derived();
+	
 
 	system("PAUSE");
 	return 0;
